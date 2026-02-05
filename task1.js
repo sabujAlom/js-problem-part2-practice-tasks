8e0605391c9e94ee6fb0d162d33ea95bb9d40a47
sabujAlom <sabujalom18@gmail.com>
2026-02-05 11:05:20 +0600

    completed task1

--- task1.js
+++ task1.js
@@ -3,3 +3,18 @@
 Find the lowest number in the array below.
 const heights2 = [167, 190, 120, 165, 137];
  */
+function numberFrequency (numbers){
+
+    let lowestNumber = numbers[0];
+    for(const number of numbers){
+        if(number < lowestNumber){
+            lowestNumber = number;
+        }
+    }
+    return lowestNumber ;
+}
+
+
+const heights2 = [167, 190, 120, 165, 137];
+console.log(numberFrequency(heights2));
+
