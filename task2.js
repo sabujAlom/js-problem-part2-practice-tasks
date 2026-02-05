a0361c1df5e2fc76617f0f7fe112be3b8f70866b
sabujAlom <sabujalom18@gmail.com>
2026-02-05 15:29:31 +0600

    finded the smallest name

--- task2.js
+++ task2.js
@@ -3,3 +3,16 @@
 Find the friend with the smallest name.
 const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
  */
+function nameFrequency (names){
+    
+    let smallestName = names[0];
+    for(const name of names){
+        if(name.length < smallestName.length){
+            smallestName = name;
+        }
+    }
+    return smallestName;
+}
+
+const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+console.log(nameFrequency(heights2))
